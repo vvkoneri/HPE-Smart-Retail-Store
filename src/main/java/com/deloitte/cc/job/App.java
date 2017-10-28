ed6819eb6eb65ce9102d8f5de6ad70826cab3ddd
Koneri <vkoneri@deloitte.com>
2017-10-28 17:35:09 +0530

    first commit

--- src/main/java/com/deloitte/cc/job/App.java
+++ src/main/java/com/deloitte/cc/job/App.java
@@ -18,7 +18,7 @@ public class App {
 			HttpResponse<String> response = Unirest
 					.post("https://deloitte02-alliance-prtnr-na05-dw.demandware.net/dw/oauth2/access_token?client_id=2b51dbad-13cb-461e-8e6e-05c058edd3ba&grant_type=urn:demandware:params:oauth:grant-type:client-id:dwsid:dwsecuretoken")
 					.header("authorization",
-							"Basic Z2F1cmF2YXJvcmE4QGRlbG9pdHRlLmNvbTpEZWwwMkAxNjM6Q2hhbmdlTWVEZWxvaXR0ZQ==")
+							"")
 					.header("content-type", "application/x-www-form-urlencoded").asString();
 
 			JSONObject jsonObj = new JSONObject(response.getBody());
